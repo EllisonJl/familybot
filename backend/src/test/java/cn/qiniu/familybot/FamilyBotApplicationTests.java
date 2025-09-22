f38d588c936c553d52ea33af2e60586647745ebc
JialuLiu <jialu.liu@foxmal.com>
2025-09-22 22:38:24 +0800

    Initial commit: upload FamilyBot project

---
+++ backend/src/test/java/cn/qiniu/familybot/FamilyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.qiniu.familybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
